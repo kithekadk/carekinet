296f164e43285bf65acdb393cf25a51ff1a484c0
unknown <dankinyi99@gmail.com>
2023-07-16 14:00:34 +0300

    refactored verify phone details page

--- lib/screens/verifyphoneno.dart
+++ lib/screens/verifyphoneno.dart
@@ -6,23 +6,29 @@ class VerifyPhone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: verifyPhoneBodyImage(context),
+      body: verifyPhoneBody(context),
     );
   }
 }
 
-Widget verifyPhoneBodyImage(BuildContext context) {
+Widget verifyPhoneBody(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
   return Column(
     children: [
-      const Center(
-          child: Image(
+      const Image(
+        fit: BoxFit.cover,
         image: AssetImage('assets/african-nurse.jpg'),
         // image: NetworkImage('https://images.unsplash.com/photo-1610632380989-680fe40816c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNvZmZlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
-      )),
+      ),
       Container(
         margin: const EdgeInsets.all(20),
-        child: const Text('Care Kinet',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        child: const Text(
+          'Care Kinet',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       const Center(
         child: Text(
@@ -36,9 +42,9 @@ Widget verifyPhoneBodyImage(BuildContext context) {
           child: Stack(
             children: [
               Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
                 width: 70,
-                child: TextField(
+                child: const TextField(
                   decoration: InputDecoration(
                     hintText: '+254',
                     border: OutlineInputBorder(),
@@ -46,8 +52,8 @@ Widget verifyPhoneBodyImage(BuildContext context) {
                 ),
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(80, 20, 0, 20),
-                child: TextField(
+                margin: const EdgeInsets.fromLTRB(80, 20, 0, 20),
+                child: const TextField(
                   decoration: InputDecoration(
                     labelText: 'Phone Number',
                     border: OutlineInputBorder(),
@@ -59,36 +65,42 @@ Widget verifyPhoneBodyImage(BuildContext context) {
           ),
         ),
       ),
-      Center(
-        child: Container(
-          width: 200,
-          height: 40,
-          child: ElevatedButton(
-            onPressed: () {
-              print('clicked');
-            },
-            child: Text('Get Code'),
-          ),
-        ),
-      ),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        ElevatedButton(
+          style:
+              ElevatedButton.styleFrom(minimumSize: Size(size.width * 0.9, 50)),
+          onPressed: () {
+            print('clicked');
+          },
+          child: const Text('Get Code'),
+        )
+      ]),
       Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: GestureDetector(
-            child: Text('Skip Registration'),
-            onTap: () {
-              print('Skip registration');
-            },
-          ),
+              child: const Text('Skip Registration'),
+              onTap: () {
+                print('Skip registration');
+              }),
         ),
       ),
-      SizedBox(
+      const SizedBox(
         height: 50,
       ),
-      Center(
-        child: Text(
-          'Already have an account? Log in'
-        ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Already have an account?'),
+          TextButton(
+              onPressed: () {
+                print('Clicked');
+              },
+              child: const Text(
+                'Log in',
+                style: TextStyle(color: Colors.green),
+              ))
+        ],
       )
     ],
   );
